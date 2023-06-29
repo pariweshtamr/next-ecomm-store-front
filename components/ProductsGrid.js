8789d6fe0eef774bd52c506cc8e42fac7168b2a6
pariwesh <tamrpariwesh@gmail.com>
2023-06-29 19:32:22 +1000

    added feature to add all products to wishlist

--- components/ProductsGrid.js
+++ components/ProductsGrid.js
@@ -1,7 +1,7 @@
 import { RevealWrapper } from "next-reveal"
 import ProductCard from "./ProductCard"
 
-const ProductsGrid = ({ products, wishedProducts }) => {
+const ProductsGrid = ({ products, wishedProducts = [] }) => {
   return (
     <div className="grid grid-cols-2 gap-y-16  place-items-center  min-lg:grid-cols-3 min-xl:!grid-cols-4 xs:grid-cols-1 ">
       {products?.length > 0 &&
--- pages/products.js
+++ pages/products.js
@@ -2,24 +2,33 @@ import Layout from "@/components/Layout"
 import dbConnect from "@/lib/mongoose"
 import Product from "@/models/Product"
 import ProductsGrid from "@/components/ProductsGrid"
+import Wishlist from "@/models/Wishlist"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./api/auth/[...nextauth]"
 
-const Products = ({ products }) => {
+const Products = ({ products, wishedProducts }) => {
   return (
     <Layout>
       <div className="w-[85%] m-[0_auto] py-10 max-w-[1440px]">
         <h2 className="title">All Products</h2>
-        <ProductsGrid products={products} />
+        <ProductsGrid products={products} wishedProducts={wishedProducts} />
       </div>
     </Layout>
   )
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(ctx) {
   await dbConnect()
+  const { user } = await getServerSession(ctx.req, ctx.res, authOptions)
   const products = await Product.find({}, null, { sort: { _id: -1 } })
+  const wishedProducts = await Wishlist.find({
+    userEmail: user.email,
+    product: products.map((p) => p._id.toString()),
+  })
   return {
     props: {
       products: JSON.parse(JSON.stringify(products)),
+      wishedProducts: wishedProducts.map((i) => i.product.toString()),
     },
   }
 }
