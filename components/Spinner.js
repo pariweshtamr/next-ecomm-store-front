1ff991a7b8f46c29057a6969f1a4e268f791c75f
pariwesh <tamrpariwesh@gmail.com>
2023-07-21 13:54:40 +1000

    minor change

--- components/Spinner.js
+++ components/Spinner.js
@@ -3,7 +3,7 @@ import { DotLoader } from "react-spinners"
 const Spinner = () => {
   return (
     <div className="w-full py-10 flex items-center justify-center">
-      <DotLoader speedMultiplier={3} color={"#555"} />
+      <DotLoader speedMultiplier={2} color={"#555"} />
     </div>
   )
 }
